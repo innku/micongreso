b9c34c3c0cb9a3dd5bc559369f74a62e7b61cf93
Federico Gonzalez <federico@innku.com>
2009-11-17 19:35:56 -0600

    Error en tipo de columnas para diputados

--- db/migrate/20091118012755_change_party_id_and_state_id_to_int.rb
+++ db/migrate/20091118012755_change_party_id_and_state_id_to_int.rb
@@ -1,11 +1,17 @@
 class ChangePartyIdAndStateIdToInt < ActiveRecord::Migration
   def self.up
-    change_column :members, :party_id, :integer
-    change_column :members, :state_id, :integer
+    remove_column :members, :party_id
+    remove_column :members, :state_id
+    
+    add_column :members, :party_id, :integer
+    add_column :members, :state_id, :integer
   end
 
   def self.down
-    change_column :members, :state_id, :string
-    change_column :members, :party_id, :string
+    remove_column :members, :state_id
+    remove_column :members, :party_id
+    
+    add_column :members, :party_id, :string
+    add_column :members, :state_id, :string
   end
 end
