5aadad147d9e3b0da47073e1d9eca085a5c4a28b
Federico Gonzalez <federico@innku.com>
2010-09-07 19:15:31 -0500

    Especificar version de will_paginate y agregar mensaje en diputados

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.9' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -19,7 +19,7 @@ Rails::Initializer.run do |config|
   config.gem "fastercsv"
   config.gem "searchlogic"
   config.gem "contacts"
-  config.gem "will_paginate"
+  config.gem "will_paginate", :version => "= 2.3.11"
   config.gem "heroku"
   config.gem 'subdomain-fu'
   
