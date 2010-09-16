f4d0d244b83578a3722307740b0b9d9e501aec7c
Federico Gonzalez <federico@innku.com>
2010-09-16 12:45:57 -0500

    Eliminar initializers, views, controladores que ya no se utilizan

--- config/initializers/subdomains.rb
+++
@@ -1,4 +0,0 @@
-SubdomainFu.tld_size = 1 # sets for current environment
-SubdomainFu.tld_sizes = {:development => 0,
-                          :test => 0,
-                          :production => 1} # set all at once (also the defaults)
